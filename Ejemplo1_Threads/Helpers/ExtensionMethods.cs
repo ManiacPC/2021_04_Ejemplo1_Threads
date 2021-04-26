c4a1c3db743c56872ac6152c56fd02233fd18fd7
Enrique Palacios <epalaciosc@pixisproti.cl>
2021-04-26 14:33:38 -0400

    Agregar archivos de proyecto.

---
+++ Ejemplo1_Threads/Helpers/ExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo1_Threads.Helpers
+{
+    public static class ExtensionMethods
+    {
+        public static int ToSeconds(this int original, int convertionRate = 1000)
+        {
+            return original * convertionRate;
+        }
+    }
+}
---
+++ Ejemplo1_Threads/Program.cs
@@ -0,0 +1,48 @@
+﻿using Ejemplo1_Threads.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ejemplo1_Threads
+{
+    /*
+     * Desarrolle una aplicación de consola C# que levante 6 hebras de ejecución.
+    Definir a cada Hebra un identificador de ejecución N desde 1 a 6
+    En la ejecución de cada una se debe hacer un sleep de N segundos y
+    luego mostrar el texto “Hola mundo” seguido del valor de N.
+
+     * */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=1; i<7; i++)
+            {
+                Thread hilo = new Thread(new ParameterizedThreadStart(Ejecutar)); // no se le pasa el parámetro (i)
+                hilo.Name = i.ToString();
+                hilo.IsBackground = true;
+
+                hilo.Start(i); // Parámetro de método ejecutar
+
+            }
+
+            Console.ReadKey();
+        }
+
+        static void Ejecutar(object o) // 3
+        {
+            Random random = new Random();
+            int randomMiliseconds = random.Next(500, 2000);            
+
+            int tiempoEnSegundos = Convert.ToInt32(o);
+
+            Console.WriteLine(tiempoEnSegundos.ToSeconds(randomMiliseconds).ToString());
+
+            Thread.Sleep(tiempoEnSegundos.ToSeconds(randomMiliseconds)); // Sleep trabaja con milisegundos (3s = 3000ms)
+            Console.WriteLine($"Hola desde el hilo {tiempoEnSegundos}");
+        }
+    }
+}
